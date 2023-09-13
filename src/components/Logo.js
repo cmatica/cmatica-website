a70275f0b32c5c67d8b87e9bc5416f73ba0bd71b
Sachin Kadam <Sachinkadamsk01@gmail.com>
2023-09-13 18:49:29 +0530

    add responsive app bar

---
+++ src/components/Logo.js
@@ -0,0 +1,8 @@
+import mbs from "../assets/cmatica_logo.png";
+import "../styles/style.css";
+
+
+const Logo = () => {
+    return <img src={mbs} className="navBarLogo" alt="logo"/>;
+};
+export default Logo;
